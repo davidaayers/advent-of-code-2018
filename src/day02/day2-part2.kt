468d4d6f05aa4f5290970d5bdd0d91b36e8a428b
David Ayers <davidaayers@gmail.com>
2018-12-02 17:57:09 -0700

    Improved efficiency of loops by returning early from a function

--- src/day02/day2-part2.kt
+++ src/day02/day2-part2.kt
@@ -2,26 +2,33 @@ package day02
 
 import readFileIntoLines
 
-data class Result(val index: Int, val answer: String?)
+data class Result(val number: Int, val answer: String?)
 
 fun main(args: Array<String>) {
     val lines: List<String> = readFileIntoLines("/day02/input.txt");
-    var cnt = 0
-    var foundAnswer = false
 
+    val (cnt, answer) = findAnswer(lines)
+    if (answer.isNullOrBlank()) {
+        println("Didn't find answer after looking at $cnt possibilities")
+    } else {
+        println("Found answer: $answer after looking at $cnt possibilities")
+    }
+}
+
+fun findAnswer(lines: List<String>): Result {
+    var cnt = 0
     lines.forEachIndexed { firstIdx, first ->
         lines.subList(firstIdx + 1, lines.size).forEach { second ->
             cnt++
 
             val (numDiffs, answer) = compareStrings(first, second)
             if (numDiffs == 1) {
-                println("Found answer: $answer first: $first, second: $second after looking at $cnt possibilities")
-                foundAnswer = true
+                println("Found answer: $answer first: $first, second: $second")
+                return Result(cnt, answer)
             }
         }
     }
-
-    if (!foundAnswer) println("Didn't find answer after looking at $cnt possibilities")
+    return Result(cnt, null)
 }
 
 fun compareStrings(first: String, second: String): Result {
