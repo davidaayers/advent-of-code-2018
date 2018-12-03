73ab4f24a6c4977505d5c4d047d736be5e45915e
David Ayers <davidaayers@gmail.com>
2018-12-02 17:45:16 -0700

    Some more cleanup

--- src/day02/day2-part2.kt
+++ src/day02/day2-part2.kt
@@ -2,30 +2,20 @@ package day02
 
 import readFileIntoLines
 
+data class Result(val index: Int, val answer: String?)
+
 fun main(args: Array<String>) {
     val lines: List<String> = readFileIntoLines("/day02/input.txt");
     var cnt = 0
     var foundAnswer = false
 
     lines.forEachIndexed { firstIdx, first ->
-        lines.subList(firstIdx, lines.size).forEach {
+        lines.subList(firstIdx + 1, lines.size).forEach { second ->
             cnt++
 
-            var numDiff = 0
-            var diffPos = -1
-            for (idx in 0 until first.length) {
-                if (first[idx] != it[idx]) {
-                    diffPos = idx
-                    numDiff++
-                    if (numDiff > 1) {
-                        return@forEach
-                    }
-                }
-            }
-
-            if (numDiff == 1) {
-                val answer = first.substring(0, diffPos) + first.substring(diffPos + 1)
-                println("Found answer: $answer first: $first, second: $it after looking at $cnt possibilities")
+            val (numDiffs, answer) = compareStrings(first, second)
+            if (numDiffs == 1) {
+                println("Found answer: $answer first: $first, second: $second after looking at $cnt possibilities")
                 foundAnswer = true
                 return@forEachIndexed
             }
@@ -33,4 +23,19 @@ fun main(args: Array<String>) {
     }
 
     if (!foundAnswer) println("Didn't find answer after looking at $cnt possibilities")
+}
+
+fun compareStrings(first: String, second: String): Result {
+    var numDiff = 0
+    var diffPos = -1
+    for (idx in 0 until first.length) {
+        if (first[idx] != second[idx]) {
+            diffPos = idx
+            numDiff++
+            if (numDiff > 1) {
+                return Result(numDiff, null)
+            }
+        }
+    }
+    return Result(numDiff, first.substring(0, diffPos) + first.substring(diffPos + 1))
 }
