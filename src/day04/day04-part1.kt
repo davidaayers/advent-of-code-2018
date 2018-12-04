4d225dc132991cc7c8be62f4dd67e904dc293422
David Ayers <davidaayers@gmail.com>
2018-12-04 14:06:59 -0700

    Solution to part 2 of puzzle

--- src/day04/day04-part1.kt
+++ src/day04/day04-part1.kt
@@ -43,6 +43,17 @@ fun main(args: Array<String>) {
     val answer = sleepiestElf.id * minute
     println("Answer (Strategy 1) to puzzle is $answer")
 
+    val sleepiestElf2 = elves.map { it.value }
+        .toList()
+        .sortedByDescending { it.greatestNumberOfTimesSleptInAMinute() }
+        .first()
+
+    val minute2 = sleepiestElf2.greatestSleepMinute()
+    println("sleepiestElf2 = $sleepiestElf2, slept the most at $minute2 " +
+            "(total of ${sleepiestElf2.greatestNumberOfTimesSleptInAMinute()} times)")
+    val answer2 = sleepiestElf2.id * minute2
+    println("Answer (Strategy 2) to puzzle is $answer2")
+
 }
 
 fun getElfId(input: String): Int {
@@ -67,10 +78,20 @@ data class Elf(val id: Int) {
     }
 
     fun greatestSleepMinute(): Int {
-        val (minute, _) = asleepTimes
-            .toList()
-            .sortedByDescending { (_, value) -> value }
-            .first()
+        val (minute, _) = findMostSleepyMinute()
         return minute
     }
+
+    fun greatestNumberOfTimesSleptInAMinute(): Int {
+        val (_, times) = findMostSleepyMinute()
+        return times
+    }
+
+    private fun findMostSleepyMinute(): Pair<Int, Int> {
+        return asleepTimes
+            .toList()
+            .sortedByDescending { (_, value) -> value }
+            .firstOrNull() ?: Pair(0,0)
+    }
+
 }
