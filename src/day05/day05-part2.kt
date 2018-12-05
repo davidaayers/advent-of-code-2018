57b5bc7a3ce4797c97968a9f8fe36c97a8ee9198
David Ayers <davidaayers@gmail.com>
2018-12-05 10:08:47 -0700

    Improvements to make slightly more FP-ish

--- src/day05/day05-part2.kt
+++ src/day05/day05-part2.kt
@@ -6,20 +6,10 @@ fun main(args: Array<String>) {
     val polymer = readEntireFile("/day05/input.txt").trim()
     //var polymer = "dabAcCaCBAcCcaDA"
 
-    var polymers = mutableListOf<String>()
+    val shortest = ('a'..'z').map { fullyReactPolymer(polymer.replace(it.toString(), "", true)) }
+        .sortedBy { it.length }
+        .first()
 
-    val distinct = polymer.toCharArray().map { it.toLowerCase() }.distinct().sorted()
-    println("distinct = $distinct")
-
-    //var alphabet = CharArray(26) { (it + 97).toChar() }.joinToString("")
-    distinct.forEach {
-        println("Removing $it")
-        polymers.add(fullyReactPolymer(polymer.replace(it.toString(), "", true)))
-    }
-
-    polymers.sortBy { it.length }
-
-    var shortest = polymers[0]
     println("Reached end, shortest polymer is $shortest")
     println("With a length of ${shortest.length}")
 }
