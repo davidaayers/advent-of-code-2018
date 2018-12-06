de65c5bd10433a9999b4e4cf63617517222ce959
David Ayers <davidaayers@gmail.com>
2018-12-06 12:13:44 -0700

    Solution to part 1

--- src/day06/day06-part1.kt
+++ src/day06/day06-part1.kt
@@ -3,19 +3,21 @@ package day06
 import readFileIntoLines
 
 fun main(args: Array<String>) {
-    val lines = readFileIntoLines("/day06/input-small.txt")
+    val lines = readFileIntoLines("/day06/input.txt")
     var name = 'A'
     val points = lines.map {
         val pair = it.parseToPair()
-        NamedPoint(name++, pair)
+        val namedPoint = NamedPoint(name++, pair)
+        println("Assigned name: ${namedPoint.name}")
+        namedPoint
     }
 
-    println("points = ${points}")
+    println("points = $points")
 
     // find the greatest number, we'll use that to build our grid
     val max = points.flatMap { it.point.toList() }
         .toList()
-        .max() ?: throw IllegalStateException()
+        .max()!!
 
     println("max = $max")
 
@@ -23,22 +25,67 @@ fun main(args: Array<String>) {
 
     // add all the points to the grid
     points.forEach {
-        grid[it.point.first][it.point.second] = it.name
+        grid[it.point.second][it.point.first] = it.name
     }
 
     printGrid(grid)
+    println("------")
+
+    // now we need to go through every point on the grid and determine
+    // which point is closest
+    val edges = mutableSetOf<Char>()
+    for (y in 0 until grid.size) {
+        for (x in 0 until grid.size) {
+            val distances = points.map {
+                Distance(it, manhattanDistance(it.point.first, x, it.point.second, y))
+            }.toList()
+                .sortedBy { it.distance }
+
+            when {
+                distances[0].distance == 0 -> grid[y][x] = distances[0].namedPoint.name
+                distances[0].distance == distances[1].distance -> grid[y][x] = '.'
+                else -> grid[y][x] = distances[0].namedPoint.name
+            }
+
+            if (x == 0 || y == 0 || x == grid.size - 1 || y == grid.size - 1) {
+                edges.add(grid[y][x])
+            }
+        }
+    }
+
+    printGrid(grid)
+
+    // now find the greatest area (probably not right due to the infinite thing
+
+    val greatestNonInfinity = grid.flatMap { it.asList() }
+        .filter { it != '.' }
+        .filter { !edges.contains(it) }
+        .groupingBy { it }
+        .eachCount()
+        .maxBy { it.value }
+
+    // now find that point in our list of points
+    val winningPoint = points.firstOrNull { it.name == greatestNonInfinity!!.key }
+
+    println("greatestNonInfinity = $greatestNonInfinity, winningPoint = $winningPoint")
+
+}
+
+fun manhattanDistance(x0: Int, x1: Int, y0: Int, y1: Int): Int {
+    return Math.abs(x1 - x0) + Math.abs(y1 - y0)
 }
 
 fun printGrid(grid: Array<Array<Char>>) {
-    for (x in 0 until grid.size) {
-        for (y in 0 until grid.size) {
-            print("${grid[x][y]}")
+    grid.flatMap { it.asList() }.forEachIndexed { idx, c ->
+        when {
+            (idx + 1) % grid.size == 0 -> println("$c")
+            else -> print("$c")
         }
-        println("")
     }
 }
 
 data class NamedPoint(var name: Char, var point: Pair<Int, Int>)
+data class Distance(var namedPoint: NamedPoint, var distance: Int)
 
 // extension function to parseToPair the line
 fun String.parseToPair(): Pair<Int, Int> {
