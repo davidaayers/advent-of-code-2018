bd66376d8c8e6251c5deff620da277998f6ae3df
David Ayers <davidaayers@gmail.com>
2018-12-11 16:00:09 -0700

    (Brute Force) Solution to part 2

--- src/day11/day11-part1.kt
+++ src/day11/day11-part1.kt
@@ -3,6 +3,7 @@ package day11
 fun main(args: Array<String>) {
     examples()
     part1()
+    part2()
 }
 
 private fun part1() {
@@ -10,10 +11,11 @@ private fun part1() {
     var winningX = 0
     var winningY = 0
     var winningValue = 0
-    for (y in 1..198) {
+    val grid = Grid(serialNumber)
+
+    for (y in 1..298) {
         for (x in 1..298) {
-            val grid = Grid(serialNumber, x, y)
-            val value = grid.value()
+            val value = grid.value(x, y)
             if (value > winningValue) {
                 winningValue = value
                 winningX = x
@@ -22,15 +24,55 @@ private fun part1() {
         }
     }
 
-    println("winning grid is $winningX,$winningY with power of $winningValue")
+    println("winning grid for part 1 is $winningX,$winningY with power of $winningValue")
 }
 
-data class Grid(val serialNumber: Int, val startX: Int, val startY: Int) {
-    fun value(size:Int = 3): Int {
+
+private fun part2() {
+    val serialNumber = 9005
+    var winningX = 0
+    var winningY = 0
+    var winningValue = 0
+    var winningSize = 0
+    val grid = Grid(serialNumber)
+
+    var size = 2
+    while (size < 298) {
+        for (y in 1..300 - size) {
+            for (x in 1..300 - size) {
+                val value = grid.value(x, y, size)
+                if (value > winningValue) {
+                    winningValue = value
+                    winningX = x
+                    winningY = y
+                    winningSize = size
+                }
+            }
+        }
+        size++
+        println("size = ${size}")
+    }
+    println("winning grid for part 2 is $winningX,$winningY with power of $winningValue and a size of $winningSize")
+}
+
+data class Grid(val serialNumber: Int) {
+
+    private val grid = Array(302) { Array(302) { 0 } }
+
+    init {
+        // build grid
+        for (y in 1..300) {
+            for (x in 1..300) {
+                grid[y][x] = cellValue(x, y)
+            }
+        }
+    }
+
+    fun value(startY: Int, startX: Int, size: Int = 3): Int {
         var value = 0
         for (y in startY until startY + size) {
             for (x in startX until startX + size) {
-                value += cellValue(x, y)
+                value += grid[x][y]
             }
         }
         return value
@@ -54,4 +96,6 @@ data class Grid(val serialNumber: Int, val startX: Int, val startY: Int) {
         return powerLevel
     }
 }
+
+
 
--- src/day11/examples.kt
+++ src/day11/examples.kt
@@ -3,15 +3,19 @@ package day11
 import java.lang.IllegalStateException
 
 fun examples() {
-    test(Grid(57,1,1).cellValue(122,79) == -5)
-    test(Grid(39,1,1).cellValue(217,196) == 0)
-    test(Grid(71,1,1).cellValue(101,153) == 4)
-    test(Grid(18,1,1).cellValue(33,45) == 4)
-    test(Grid(18,1,1).cellValue(34,45) == 4)
-    test(Grid(18,1,1).cellValue(35,45) == 4)
-    test(Grid(18,1,1).cellValue(33,46) == 3)
-    test(Grid(18,1,1).cellValue(34,46) == 3)
-    test(Grid(18,1,1).cellValue(35,46) == 4)
+    var grid = Grid(57)
+    test(grid.cellValue(122,79) == -5)
+    grid = Grid(39)
+    test(grid.cellValue(217,196) == 0)
+    grid = Grid(71)
+    test(grid.cellValue(101,153) == 4)
+    grid = Grid(18)
+    test(grid.cellValue(33,45) == 4)
+    test(grid.cellValue(34,45) == 4)
+    test(grid.cellValue(35,45) == 4)
+    test(grid.cellValue(33,46) == 3)
+    test(grid.cellValue(34,46) == 3)
+    test(grid.cellValue(35,46) == 4)
 }
 
 fun test(bool: Boolean) {
