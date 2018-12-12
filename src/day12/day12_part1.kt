e4f9bef0558affb0501a1e4cf01dda6642f7453b
David Ayers <davidaayers@gmail.com>
2018-12-12 10:55:41 -0700

    Solution to day 1

--- src/day12/day12_part1.kt
+++ src/day12/day12_part1.kt
@@ -3,15 +3,39 @@ package day12
 import readFileIntoLines
 
 fun main(args: Array<String>) {
-    val (initial, rules) = parseInput("/day12/input-small.txt")
+    val (initial, rules) = parseInput("/day12/input.txt")
 
-    val pots = initial.toCharArray().map { it.toString() }.toMutableList()
+    var pots = initial.toCharArray().map { it.toString() }.toMutableList()
     // add a few empty pots to the left & right
-    repeat(4) {
-        pots.add(0,".")
+    repeat(5) {
+        pots.add(0, ".")
         pots.add(".")
     }
     println("pots = $pots")
+
+    val zeroIndex = pots.indexOfFirst { it == "#" }
+    val gens = 20
+
+    println("0:\t${pots.joinToString(separator = "")}")
+    for (gen in 1..gens) {
+        val nextGen = mutableListOf<String>()
+        nextGen.addAll(pots)
+        for (pot in 2 until pots.size - 2) {
+            val checkPot = (pot - 2..pot + 2).joinToString(separator = "") { pots[it] }
+            nextGen[pot] = rules.getOrDefault(checkPot, ".")
+        }
+        // do we need to add some pots to the beginning or end?
+        repeat(3) {
+            nextGen.add(".")
+        }
+        //zeroIndex += 3
+        pots = nextGen
+        println("$gen:\t${pots.joinToString(separator = "")}")
+    }
+
+    // get the total of our pots
+    val sum = pots.mapIndexed { idx, it -> if (it == ".") 0 else idx - zeroIndex }.sum()
+    println("sum = $sum")
 
 }
 
@@ -19,16 +43,13 @@ fun parseInput(fileName: String): Pair<String, MutableMap<String, String>> {
     val iterator = readFileIntoLines(fileName).iterator()
 
     val initial = iterator.next().substring(15)
-    println("initial = [${initial}]")
 
     val rules = mutableMapOf<String, String>()
     iterator.forEachRemaining {
         if (it.trim().isNotEmpty()) {
-            val matchResult = """(\W{4}) => (\W)""".toRegex().find(it)
+            val matchResult = """(\W{5}) => (\W)""".toRegex().find(it)
             val (rule, result) = matchResult!!.destructured
-            println("line = $it")
-            println("rule = ${rule} result = $result")
-            if ("#".equals(result)) {
+            if ("#" == result) {
                 rules[rule] = result
             }
         }
