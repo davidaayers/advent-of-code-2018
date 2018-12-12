6dfd23968617a79e96c6d054e88f926a3f32f72f
David Ayers <davidaayers@gmail.com>
2018-12-12 11:37:41 -0700

    Answer to part2, with much help from reddit

--- src/day12/day12_part1.kt
+++ src/day12/day12_part1.kt
@@ -3,7 +3,7 @@ package day12
 import readFileIntoLines
 
 fun main(args: Array<String>) {
-    val (initial, rules) = parseInput("/day12/input-small.txt")
+    val (initial, rules) = parseInput("/day12/input.txt")
 
     var pots = initial.toCharArray().map { it.toString() }.toMutableList()
     // add a few empty pots to the left & right
@@ -14,9 +14,12 @@ fun main(args: Array<String>) {
     //println("pots = $pots")
 
     val zeroIndex = pots.indexOfFirst { it == "#" }
-    val gens = 20
+    val gens = 50000000000
+
+    var lastDiffs = mutableListOf<Int>()
 
     println("0:\t${pots.joinToString(separator = "")}")
+    var prevGenSum = sum(pots, zeroIndex)
     for (gen in 1..gens) {
         val nextGen = mutableListOf<String>()
         nextGen.addAll(pots)
@@ -31,13 +34,34 @@ fun main(args: Array<String>) {
         }
 
         pots = nextGen
-        println("$gen:\t${pots.joinToString(separator = "")}")
+        //println("$gen:\t${pots.joinToString(separator = "")}")
 
-        val sum = pots.mapIndexed { idx, it -> if (it == ".") 0 else idx - zeroIndex }.sum()
-        println("sum for gen $gen = $sum")
+        val sum = sum(pots, zeroIndex)
+        var diff = sum - prevGenSum
+        prevGenSum = sum
+        println("sum for gen $gen = $sum, diff = $diff")
+
+        if (gen % 20.toLong() == 0.toLong()) {
+            println("Answer to part1 = $sum")
+        }
+
+        lastDiffs.add(0, diff)
+        if (lastDiffs.size > 10) {
+            lastDiffs = lastDiffs.subList(0, 10)
+
+            if (lastDiffs.distinct().toList().size == 1) {
+                var finalAnswer = sum + ((gens - gen) * diff)
+                println("list hasn't changed in 10 iterations, achieved stability...answer to part 2= $finalAnswer")
+                break
+            }
+        }
+
     }
 
 }
+
+private fun sum(pots: MutableList<String>, zeroIndex: Int) =
+    pots.mapIndexed { idx, it -> if (it == ".") 0 else idx - zeroIndex }.sum()
 
 fun parseInput(fileName: String): Pair<String, MutableMap<String, String>> {
     val iterator = readFileIntoLines(fileName).iterator()
