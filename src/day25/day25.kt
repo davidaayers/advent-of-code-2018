63de67348a48b4a547349588da9d685ca4863264
David Ayers <davidaayers@gmail.com>
2018-12-26 12:36:01 -0700

    WIP for day 25

--- src/day25/day25.kt
+++ src/day25/day25.kt
@@ -3,8 +3,31 @@ package day25
 import readFileIntoLines
 
 fun main(args: Array<String>) {
-    val points = parseData("/day25/input-small1.txt")
-    println("points = ${points}")
+    if (findAllConstellations(parseData("/day25/input-small1.txt")).size != 2) {
+        throw IllegalStateException()
+    }
+    if (findAllConstellations(parseData("/day25/input-small2.txt")).size != 4) {
+        throw IllegalStateException()
+    }
+    val constellations = findAllConstellations(parseData("/day25/input-small3.txt"))
+    if (constellations.size != 3) {
+        throw IllegalStateException()
+    }
+
+}
+
+private fun findAllConstellations(points: List<Point4d>): MutableList<Constellation> {
+    val constellations = mutableListOf<Constellation>()
+
+    points.forEach { point ->
+        val existing = constellations.find { point in it }
+        if (existing != null) {
+            existing.points.add(point)
+        } else {
+            constellations.add(Constellation().apply { this.points.add(point) })
+        }
+    }
+    return constellations
 }
 
 fun parseData(fileName: String): List<Point4d> {
@@ -22,4 +45,15 @@ data class Point4d(val x: Int, val y: Int, val z: Int, val t: Int) {
                 Math.abs(other.z - this.z) +
                 Math.abs(other.t - this.t))
     }
+}
+
+class Constellation {
+    val points = mutableListOf<Point4d>()
+
+    // a point is "in" the constellation if at least one of the
+    // other points is within manhattan distance of 3
+    operator fun contains(point: Point4d): Boolean {
+        return points.any { it.manhattanDistanceTo(point) <= 3 }
+    }
+
 }
